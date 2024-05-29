4c5220b3ba7db568240b4230b817547eb48e5643
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-05-29 21:13:25 +0700

    fixing bugs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express")
-require("dotenv").config()
+
 const cors = require("cors")
 const mongoose = require("mongoose")
 const timerRoute = require("./Routes/timerRoute")
-
+require("dotenv").config()
 const app = express()
 
 app.use(cors())
@@ -12,7 +12,10 @@ app.use(cors())
 app.use(express.json())
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("App connected to database")
   })
