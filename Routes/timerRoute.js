32a7f308f43420ad5b4016127fa491bbb7cae893
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-06-02 15:54:14 +0700

    add get timers

--- Routes/timerRoute.js
+++ Routes/timerRoute.js
@@ -2,6 +2,21 @@ const express = require("express")
 const Timer = require("../models/timerModel.js")
 const router = express.Router()
 
+router.get("/", async (req, res) => {
+  try {
+    const timer = await Timer.find({})
+
+    return res.status(201).json({
+      data: timer,
+    })
+
+    // console.log("oke bisa works")
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send({ message: error.message })
+  }
+})
+
 router.post("/create-timer", async (req, res) => {
   try {
     if (!req.body.uuid || !req.body.setTimer) {
